55b0be4cdff26887434ed0d2d796ef485678be40
Richard <richardoda@gmail.com>
2023-06-03 01:06:59 -0700

    implemented renderTweets function where it takes a database of tweets and put it into the correct CSS style

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,17 +11,37 @@
 $(document).ready(function() {
   console.log("Client.js scripts are up and running");
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    for(let i of tweets){
+      const createTweet = createTweetElement(i)
+      $("main").append(createTweet)
+    }
+  }
 
   const createTweetElement = function(data) {
     const markup = `
@@ -53,10 +73,6 @@ $(document).ready(function() {
     return markup;
   };
 
-  //const $tweet = createTweetElement(tweetData);
-  const $tweet = createTweetElement(tweetData);
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('main').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 });
 
