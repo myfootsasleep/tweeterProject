c49c928ba5f3bcce6e6b18b9a876fcbf5ea3f524
Richard <richardoda@gmail.com>
2023-06-03 03:19:12 -0700

    implemented form submission jquery, created a POST request and a GET request, so hard coded data is no longer required

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -12,28 +12,7 @@ $(document).ready(function() {
   console.log("Client.js scripts are up and running");
 
   const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
+
   ]
 
   const renderTweets = function(tweets) {
@@ -73,6 +52,16 @@ $(document).ready(function() {
     return markup;
   };
 
-  renderTweets(data);
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    $.post("/tweets", $(this).serialize());
+  });
+  const loadTweets = () => {
+    $.ajax("/tweets")
+    .then(function(tweets){
+      renderTweets(tweets)
+    })
+   }
+  loadTweets()
 });
 
